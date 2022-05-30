7e14ef9f1b48db0e856a8ce7328b958cfa15f4d1
Enrico <bravienrico@yahoo.it>
2022-05-30 15:58:45 +0200

    added reader for report queue

---
+++ kafka_connector/report_reader.py
@@ -0,0 +1,38 @@
+from confluent_kafka import Consumer
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+def consumer():
+    properties = {}
+    for property in config["kafka_consumer"].keys():
+        properties[property] = config["kafka_consumer"][property]
+
+    kafka_consumer = Consumer(properties)
+
+    topics = [config["kafka_topics"]["attestation_report_topic"]]
+    kafka_consumer.subscribe(topics)
+
+    print("Start consuming reports...")
+    
+    while True:
+        msg = kafka_consumer.poll(1.0) # every second check if there is some message
+
+        if msg is None:
+            print("No message found!")
+            continue
+        if msg.error():
+            print("Consumer error: {}".format(msg.error()))
+            continue
+        if msg.topic() is None:
+            print("Received message has None topic")
+            continue
+
+        str = msg.value().decode('utf-8')
+        print("Message value: %s", str)
+
+
+if __name__ == "__main__":
+
+    consumer()
