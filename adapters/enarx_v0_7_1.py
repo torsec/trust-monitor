9f3077f3485a9e228ae6ce4929d670fe07744a8e
fulmicotone98 <jacopo.catalano98@gmail.com>
2024-03-18 16:44:59 +0100

    Multiple hashes in whitelist supported. Multiple WASM workloads can be attested on the same Enarx machine by the same TM attestation thread

--- adapters/enarx_v0_7_1.py
+++ adapters/enarx_v0_7_1.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import threading
 from cryptography import x509
 from cryptography.hazmat.primitives.asymmetric import (ec, utils)
 from cryptography.hazmat.primitives import hashes
@@ -27,8 +28,6 @@ att_server_timeout = int(config["enarx_wasm_att_service"]["timeout"])
 
 att_result = None # Used to check if the attestation has been done (att_result = bool(True) or bool(False))
 allowed_wasm_hashes = []
-run_wasm_hash = ""
-attempted_wasm_hash = ""
 
 class AttestationServer(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -47,17 +46,26 @@ class AttestationServer(BaseHTTPRequestHandler):
         # Read all bytes received
         all_bytes = self.rfile.read(content_length)
         
-        # Take the 1st byte which is the size (number of bytes) of the signature
-        bytes_size_signature = all_bytes[0:1]
+        # Take the 1st byte which is the size (number of bytes) of the .wasm hash
+        bytes_size_hash = all_bytes[0:1]
+        num_bytes_hash = int.from_bytes(bytes_size_hash, "little")
+        print("Size in bytes of the .wasm hash: " + str(num_bytes_hash))
+        
+        # Take the 2nd byte which is the size (number of bytes) of the .wasm signature
+        bytes_size_signature = all_bytes[1:2]
         num_bytes_signature = int.from_bytes(bytes_size_signature, "little")
         print("Size in bytes of the signed .wasm: " + str(num_bytes_signature))
         
-        # Take the signature
-        bytes_signature = all_bytes[1:(num_bytes_signature+1)]
+        # Take the hash of the .wasm
+        bytes_hash = all_bytes[2:(num_bytes_hash+2)]
+        print("Hash of the .wasm: " + bytes_hash.hex() + "\n")
+        
+        # Take the signature of the .wasm
+        bytes_signature = all_bytes[(num_bytes_hash+2):(num_bytes_hash+2+num_bytes_signature)]
         print("Signature on the .wasm: " + bytes_signature.hex() + "\n")
 
         # Take the certificate of the Keep (bytes)
-        bytes_cert = all_bytes[(num_bytes_signature+1):(content_length+1)]
+        bytes_cert = all_bytes[(num_bytes_hash+2+num_bytes_signature):(content_length+1)]
 
         # Parse the certificate bytes into Certificate object
         x509_cert = x509.load_der_x509_certificate(bytes_cert)
@@ -79,38 +87,43 @@ class AttestationServer(BaseHTTPRequestHandler):
         
         # Get the public key from the certificate
         pubkey = x509_cert.public_key()
+        hash_str = bytes_hash.hex()
+        print("WASM hash: " + hash_str + "\n")
         try:
-                for hash in allowed_wasm_hashes:
-                    
-                    wasm_hash_bytes = bytes.fromhex(hash)
-                    
-                    global run_wasm_hash
-                    run_wasm_hash = hash
-                    
+                if hash_str in allowed_wasm_hashes:
+                    print("WASM hash is in the whitelist!")                
+
                     # SHA256 (Intel CPU)
-                    if len(wasm_hash_bytes) == 32:
+                    if len(bytes_hash) == 32:
                         # Verify the signature over the .wasm's hash with the public key
-                        
-                        if  pubkey.verify(bytes_signature, wasm_hash_bytes, ec.ECDSA(utils.Prehashed(hashes.SHA256()))) == None:
-                            break
-                    
+                        if pubkey.verify(bytes_signature, bytes_hash, ec.ECDSA(utils.Prehashed(hashes.SHA256()))) == None:
+                            att_result = bool(True) # Record successful attestation
+                            print("Attestation: " + att_result.__str__())
+                            self.send_response(200, "Certificate successfully received and signature over the .wasm verified!")
+                            self.end_headers()
+                            return
+
                     # SHA384 (AMD CPU)
-                    if len(wasm_hash_bytes) == 48:
+                    if len(bytes_hash) == 48:
                         # Verify the signature over the .wasm's hash with the public key
-                        
-                        if  pubkey.verify(bytes_signature, wasm_hash_bytes, ec.ECDSA(utils.Prehashed(hashes.SHA384()))) == None:
-                            break
+                        if pubkey.verify(bytes_signature, bytes_hash, ec.ECDSA(utils.Prehashed(hashes.SHA384()))) == None:
+                            att_result = bool(True) # Record successful attestation
+                            print("Attestation: " + att_result.__str__())
+                            self.send_response(200, "Certificate successfully received and signature over the .wasm verified!")
+                            self.end_headers()
+                            return
+                else:
+                    print("Error: WASM hash is not in the whitelist!")
+                    self.send_response(400, "Bad Request!")
+                    self.end_headers()
+                    return
                 
-                att_result = bool(True) # Record successful attestation
-                print("Attestation: " + att_result.__str__())
-                self.send_response(200, "Certificate successfully received and signature over the .wasm verified!")
-                self.end_headers()
         except:
                 att_result = bool(False) # Record unsuccessful attestation with invalid signature
                 print("Error: Invalid Signature exception!")
                 self.send_response(400, "Bad Request!")
                 self.end_headers()
-
+                
 class EnarxAdapter():
 
     def __init__(self) -> None:
@@ -123,6 +136,10 @@ class EnarxAdapter():
         pass
 
     def attest(entity, _steward, whitelist, _se, topic):
+        
+        def stop_server():
+            print("Stopping server...")
+            webServer.shutdown()
         
         if tech not in entity["att_tech"]:
             return {"error" : tech + " is not present into the entity's attestation technologies list"}
@@ -139,15 +156,20 @@ class EnarxAdapter():
         webServer = HTTPServer((hostname, port), AttestationServer)
         webServer.socket.settimeout(att_server_timeout) # Set the server timeout to 5 mins
         
+        # Create a timer that will stop the server after a certain time
+        shutdown_timer = threading.Timer(att_server_timeout, stop_server)
+        
+        # Start the timer
+        shutdown_timer.start()
+        
         print("TM Enarx Attestation Server started at http://%s:%s" %(hostname, port))
                 
-        webServer.handle_request()
+        webServer.serve_forever()
         
         if att_result == True:
             run_kafka_producer( {
                 "entity_uuid": entity["entity_uuid"],
                 "att_tech": tech,
-                # "run_wasm_hash": run_wasm_hash,
                 "trust": True
             }, topic )
         
@@ -155,7 +177,6 @@ class EnarxAdapter():
             run_kafka_producer( {
                 "entity_uuid": entity["entity_uuid"],
                 "att_tech": tech,
-                # "attempted_wasm_hash": run_wasm_hash,
                 "trust": False
             }, topic )
             
@@ -163,7 +184,7 @@ class EnarxAdapter():
         if att_result == None:
             print("WASM Attestation Server Timeout Expired! - Server shutdown.")
         else:
-            print("Attestation done! - Server Shutdown.")
+            print("Attestation phase done! - Server Shutdown.")
 
     def delete(entity, verifier):
         """
